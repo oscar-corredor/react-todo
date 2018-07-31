a165f705a3a27656e223002abbebcbbe18c0d497
Oscar Corredor <oskar132@gmail.com>
2018-07-31 18:52:12 +0200

    Project setup done, ready to start implementing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GeneralOverView from './containers/GeneralOverview/GeneralOverview'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GeneralOverView />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/GeneralOverview/GeneralOverview.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class GeneralOverview extends Component {
+  state = {  }
+  render() { 
+    return ( <p>Hue hue</p> );
+  }
+}
+ 
+export default GeneralOverview;
